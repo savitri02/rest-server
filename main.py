b0b761772f027a7e0b0e7f9345bd6d05df11fc7e
savitri02 <edvard.grmadnik@guest.arnes.si>
2024-12-08 19:55:51 +0100

    fix error to save schema creation

--- main.py
+++ main.py
@@ -376,7 +376,7 @@ def update_schema(resource_name):
     try:
         schema = request.get_json()
         # Validate that it's a valid JSON Schema
-        validate({}, schema)  # Simple validation to ensure it's a valid schema
+      # eg_ validate({}, schema)  # Simple validation to ensure it's a valid schema
         SchemaHandler.save_schema(resource_name, schema)
         return jsonify(schema)
     except Exception as e:
